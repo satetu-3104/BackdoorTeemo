9191a84abf63298403955b387e62a7c98cdb100e
satetu <aoi3104aoi@gmail.com>
2025-05-06 15:35:24 +0900

    deploy to GitHub Pages

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/BackdoorTeemo/sw.js', { scope: '/BackdoorTeemo/' })})}
